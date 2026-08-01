926fc9a9cb688ea2774cd052235bea32856dffbe
Ebrahim <abnjawher25@gmail.com>
2026-08-01 23:24:36 +0300

    add more projects for Algorithms-fundamentals

---
+++ 28-SumOddNumbersFrom1To(N)/pro28SumOddNumbersFrom1To(N)/pro28SumOddNumbersFrom1To(N).cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+
+using namespace std;
+
+// This program calculates the sum of odd numbers from 1 to N, where N is a user-defined range.
+enum enAllCases { Odd = 1,Even=2 };
+
+// Function to read the number of range from the user
+short readNnmberOfRange()
+{
+	// Prompt the user to enter the number of range
+    short Number;
+
+	// Read the number of range from the user
+    cout << "Enter the number of range: ";
+    cin >> Number;
+
+	// Validate the input to ensure it is a positive number
+    while (Number <= 0)
+    {
+        cout << "Please enter a positive number: ";
+        cin >> Number;
+    }
+
+	// Return the validated number
+    return Number;
+
+
+}
+
+// Function to check if a number is odd or even
+enAllCases CheckOddNumbers(short Number)
+  {
+    if (Number % 2 != 0)
+        return enAllCases::Odd;
+    else
+        return enAllCases::Even;
+
+  }
+
+// Function to calculate the sum of odd numbers from 1 to N
+short printSumLoopOfNumbers(short Number)
+{
+    short sum = 0;
+    for (short i = 1; i <= Number; i++)
+      {
+        if (CheckOddNumbers(i) == enAllCases::Odd)
+            sum += i;
+
+      }
+       
+	// Return the calculated sum of odd numbers
+    return sum;
+
+}
+
+// Main function to execute the program
+int main()
+{
+	// Read the number of range from the user
+    short N = readNnmberOfRange();
+
+	// Calculate and print the sum of odd numbers from 1 to N
+    cout<<printSumLoopOfNumbers(N);
+
+	// Return 0 to indicate successful execution
+    return 0;
+}
+
---
+++ 30-FactorialOfNumbers/pro30FactorialOfNumbers/pro30FactorialOfNumbers.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+// Function to read a number from the user
+short readNumberOfFactorial()
+  {
+	// Variable to store the number of factorial    
+    short NumberOfFactorial;
+
+	// Prompt the user to enter a number    
+    cout << "Please enter a number to get a factorial: ";
+    cin >> NumberOfFactorial;
+
+	// Return the number entered by the user
+    return NumberOfFactorial;
+
+  }
+
+// Function to calculate the factorial of a number
+int GetAFactorial(short NumberOfFactorial)
+  {
+	// Variable to store the factorial result
+    int factorial =1 ;
+
+	// Loop to calculate the factorial of the number
+    for (short i = 1; i <= NumberOfFactorial; i++)
+        factorial *= i;
+
+	// Return the calculated factorial
+    return factorial;
+  }
+
+int main()
+{
+	// Read the number of factorial from the user
+    short N = readNumberOfFactorial();
+
+	// Calculate and display the factorial of the number
+    cout << GetAFactorial(N);
+    return 0;
+}
+
